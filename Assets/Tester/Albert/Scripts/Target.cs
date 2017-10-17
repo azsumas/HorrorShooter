4ca5cf5464283fd588eb30cbe5cbe4ee0511b732
Albert Rodriguez Espada <albertrodriespada@gmail.com>
2017-10-17 16:58:17 +0200

    Unity version 17/10

---
+++ Assets/Tester/Albert/Scripts/Target.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public float health = 100f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
