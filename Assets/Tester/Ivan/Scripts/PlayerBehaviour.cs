eee3735022b6eda601400114160f1d6f37eb6cf6
Albert Rodriguez Espada <albertrodriespada@gmail.com>
2017-10-19 02:31:42 +0200

    Unity ReUpload II

---
+++ Assets/Tester/Ivan/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+    {
+    private CharacterController controller;
+	[Header("Direction")]
+    public Vector3 moveDirection;
+	[Header("Speed")]
+    public float speed;
+	public float run;
+    public float slowStep;
+    public Vector2 axis;
+	Vector3 desiredDirection;
+	[Header("Stats Jumps")]
+    public float forceToGround = Physics.gravity.y;
+    public float jumpSpeed;
+    public float gravitymagnitude = 1;
+    [Header("Stats")]
+    public bool jump;
+    public bool isGrounded;
+
+    // Use this for initialization
+    void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Reset states
+        if (!controller.isGrounded) isGrounded = false;
+
+        //logic
+        if (isGrounded && !jump)
+        {
+            moveDirection.y = forceToGround;
+        }
+        else
+        {
+            moveDirection.y += forceToGround * Time.deltaTime;
+        }
+        moveDirection.y += Physics.gravity.y * Time.deltaTime;
+
+        desiredDirection = transform.forward * axis.y + transform.right * axis.x;
+        moveDirection.x = desiredDirection.x * speed;
+        moveDirection.z = desiredDirection.z * speed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+	}
+
+    public void SetHorizontalAxis(float x)
+    {
+        axis.x = x;
+    }
+
+    public void SetVerticalAxis(float y)
+    {
+        axis.y = y;
+    }
+
+    public void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            //sound.Play(0);
+            jump = true;
+            moveDirection.y = jumpSpeed;
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            if (controller.isGrounded)
+            {
+                isGrounded = true;
+                jump = false;
+            }
+        }
+    }
+
+    public void Run()
+    {
+        speed += run;
+    }
+
+    public void Walk()
+    {
+        speed -= run;
+    }
+}
