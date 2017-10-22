a342ba1113c21af08f94e2c2e1134d6408509179
Albert Rodriguez Espada <albertrodriespada@gmail.com>
2017-10-22 02:05:16 +0200

    Enemy 0.1

---
+++ Assets/EnemySight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySight : MonoBehaviour {
+    public float fieldOfViewAngle = 110f;
+    public bool playerInSight;
+    public Vector3 personalLastSighting;
+
+    private NavMeshAgent nav;
+    private SphereCollider col;
+    private Animator anim;
+    private LastPlayerSighting last playerSighting;
+    private GameObject player;
+    private PlayerHealth playerHealth;
+    private Vector3 previousSighting;
+}
