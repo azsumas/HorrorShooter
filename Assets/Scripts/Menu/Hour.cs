215404940fa140b58ee32b8d912dff5ee757a34e
Ivan <iescario149@gmail.com>
2017-10-28 14:10:25 +0200

    Prototype of MainMenu
    Prototype:
    - Title screen(Title, Hour, Name, Button next screen)
    - Main Screen(Show your name and Hour)

---
+++ Assets/Scripts/Menu/Hour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hour : MonoBehaviour
+{
+    public Text time;
+    string hour;
+    // Use this for initialization
+
+    void FixedUpdate()
+    {
+        time.text = System.DateTime.Now.ToString("HH:mm:ss dd/MM/" + "2134");
+    }
+}
---
+++ Assets/Scripts/Menu/YourName.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YourName : MonoBehaviour
+{
+    public Text name;
+    public MainMenu nameMenu;
+
+    public void Welcome()
+    {
+        name.text = "Welcome, < " + nameMenu.userName + " >";
+    }
+}
