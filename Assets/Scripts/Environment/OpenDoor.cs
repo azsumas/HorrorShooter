1ddd0d7226692ec19c65c18813af1d29e1764adb
Ivan <iescario149@gmail.com>
2017-11-08 14:51:04 +0100

    Automatic Sliding Doors.

--- Assets/Scripts/Environment/OpenDoor.cs
+++ Assets/Scripts/Environment/OpenDoor.cs
@@ -4,48 +4,25 @@ using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
-    bool open = false;
-    [Header("Open speed and Position")]
-    public GameObject text;
-    public float openSpeed;
-    public float maxOpen;
-    public float posX;
-    public float PosZ;
+    public Animator anim;
 
-    // Use this for initialization
-    void Update()
+    void Start()
     {
-        if(open == true)
-        {
-            posX += openSpeed;
-            if (posX > maxOpen) posX = maxOpen;
-
-            this.gameObject.transform.position = new Vector3(posX, 0, PosZ);
-        }
-        if (open == false)
-        {
-            posX -= openSpeed;
-            if (posX <= 0) posX = 0;
-            this.gameObject.transform.position = new Vector3 (posX, 0, PosZ);
-        }
+        anim.GetComponent<Animator>();
     }
 
-    void OnTriggerStay(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            text.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E)) open = true;
-        }
+        anim.SetTrigger("OpenDoors");
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            text.SetActive(true);
-            open = false;
-            text.SetActive(false);
-        }
+        anim.enabled = true;
+    }
+
+    void PauseAnimationEvent()
+    {
+        anim.enabled = false;
     }
 }
