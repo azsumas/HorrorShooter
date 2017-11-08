3746f55c2d1d1bf092c57275e92b8a5329fd0dd4
Ivan <iescario149@gmail.com>
2017-11-08 20:15:06 +0100

    Open doors with buttons.
    - New script for open doors with button ( no automatic )
    - Add new meshes to level (doors, control open doors)

---
+++ Assets/Scripts/Environment/OpenDoorButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorButton : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject text;
+    float time;
+    public float maxTime;
+    bool canOpenDoor;
+
+    void Start()
+    {
+        anim.GetComponent<Animator>();
+        canOpenDoor = false;
+    }
+
+    void Update()
+    {
+        if (canOpenDoor == true)
+        {
+            time += Time.deltaTime;
+            if (time >= maxTime)
+            {
+                anim.enabled = true;
+                canOpenDoor = false;
+                time = 0;
+                return;
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        text.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("OpenDoors");
+            canOpenDoor = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        text.SetActive(false);
+    }
+
+    void PauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+}
