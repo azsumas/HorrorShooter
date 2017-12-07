7e20d0f8f8a56a82dae273442a8c5a7b100625d8
Ivan <iescario149@gmail.com>
2017-12-07 18:42:25 +0100

    EndScreen in progress

---
+++ Assets/Scripts/Menu/EndScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScreen : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
