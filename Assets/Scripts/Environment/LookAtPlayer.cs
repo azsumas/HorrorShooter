2a80f8653752223162a0fe34e584e7e6ebea9ec1
Ivan <iescario149@gmail.com>
2017-12-14 20:51:56 +0100

    LightingRotationandLookPlayer (Albert)

---
+++ Assets/Scripts/Environment/LookAtPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour {
+    public Transform targetTransform;
+
+    // Use this for initialization
+    void Start () {
+        targetTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(targetTransform);
+    }
+}
