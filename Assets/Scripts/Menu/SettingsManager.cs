cafc1ae29b04233b6b25bf37d89b2af54b732eda
Ivan <iescario149@gmail.com>
2018-01-21 18:26:49 +0100

    Fixed some errors in graphics options
    - Only 3 resolutions availables.
    - Default is HD Res.

--- Assets/Scripts/Menu/SettingsManager.cs
+++ Assets/Scripts/Menu/SettingsManager.cs
@@ -8,71 +8,48 @@ using UnityEngine.Audio;
 public class SettingsManager : MonoBehaviour
 {
     public Toggle fullScreenToggle;
-    public Dropdown resolutionDrop;
     public Dropdown qualityDrop;
     public Button applyButton;
     public AudioMixer audioMixer;
 
-    public Resolution[] resolutions;
     public GameSettings gameSettings;
 
     void Start()
     {
         OnFullScreenChange();
-        resolutionDrop.RefreshShownValue();
+        HDRes();
     }
     void OnEnable()
     {
         gameSettings = new GameSettings();
 
         fullScreenToggle.onValueChanged.AddListener(delegate { OnFullScreenChange(); });
-        resolutionDrop.onValueChanged.AddListener(delegate { OnResulitionChange(); });
         qualityDrop.onValueChanged.AddListener(delegate { OnQualityChange(); });
-        applyButton.onClick.AddListener(delegate { OnApplyButtonClick(); });
-
-        resolutions = Screen.resolutions;
-        foreach(Resolution resolution in resolutions)
-        {
-            resolutionDrop.options.Add(new Dropdown.OptionData(resolution.ToString()));
-        }
-        //LoadSettings();
     }
 
     public void OnFullScreenChange()
     {
         gameSettings.fullScreen =  Screen.fullScreen = fullScreenToggle.isOn;
     }
-
-    public void OnResulitionChange()
+    public void FullHDRes()
     {
-        Screen.SetResolution(resolutions[resolutionDrop.value].width, resolutions[resolutionDrop.value].height, Screen.fullScreen);
-        gameSettings.resolution = resolutionDrop.value;
+        Screen.SetResolution(1920, 1080, Screen.fullScreen);
+    }
+
+    public void MidFullRes()
+    {
+        Screen.SetResolution(1600, 1200, Screen.fullScreen);
+    }
+
+    public void HDRes()
+    {
+        Screen.SetResolution(1280, 720, Screen.fullScreen);
     }
 
     public void OnQualityChange()
     {
         QualitySettings.masterTextureLimit = gameSettings.quality = qualityDrop.value;
     }
-
-    public void OnApplyButtonClick()
-    {
-        //SaveSettings();
-    }
-
-   /* public void SaveSettings()
-    {
-        string jsonData = JsonUtility.ToJson(gameSettings, true);
-        File.WriteAllText(Application.persistentDataPath + "/gamesttings.json", jsonData);
-    }
-
-    public void LoadSettings()
-    {
-        gameSettings = JsonUtility.FromJson<GameSettings>(File.ReadAllText(Application.persistentDataPath + "/gamesettings.json"));
-
-        fullScreenToggle.isOn = gameSettings.fullScreen;
-        resolutionDrop.value = gameSettings.resolution;
-        qualityDrop.value = gameSettings.resolution;
-    }*/
 
     public void SetVolume(float volume)
     {
