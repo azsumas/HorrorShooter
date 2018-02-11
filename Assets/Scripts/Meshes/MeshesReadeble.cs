a3e214360887dabe8902396c226dac877a341a5f
HectorBonada <hectorbonada@gmail.com>
2018-02-11 22:09:25 +0100

    Error of the colliders solved

---
+++ Assets/Scripts/Meshes/MeshesReadeble.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshesReadeble : MonoBehaviour {
+
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        print(mesh.isReadable);
+    }
+}
