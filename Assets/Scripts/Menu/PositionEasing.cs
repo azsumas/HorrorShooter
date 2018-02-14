1741d0977087652619f74d26eacbc4c1831e5c6a
Ivan <iescario149@gmail.com>
2018-02-15 00:08:44 +0100

    Easings title screen

---
+++ Assets/Scripts/Menu/PositionEasing.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionEasing : MonoBehaviour
+{
+    public float iniPos;
+    public float finalPos;
+    public float currentTime;
+    public float duration;
+
+    // Update is called once per frame
+    public void Start()
+    {
+        transform.position = new Vector3(transform.position.x, iniPos, transform.position.z);
+    }
+
+    void Update ()
+    {
+        if(currentTime >= 0)
+        {
+            float value = Easing.BounceEaseOut(currentTime, iniPos, finalPos - iniPos, duration);
+            transform.localPosition = new Vector3(transform.position.x, value, transform.position.z);
+        }
+
+        currentTime += Time.deltaTime;
+
+        if(currentTime >= duration)
+        {
+            transform.localPosition = new Vector3(transform.position.x, finalPos, transform.position.z);
+        }
+    }
+}
+
+
