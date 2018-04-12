6a9089d20b0ee7525107bebfe8bcc16ce15aa7a9
Ivan <iescario149@gmail.com>
2018-04-12 21:05:41 +0200

    End Screen buttons improved

---
+++ Assets/Scripts/Menu/ButtonsSelectedEnd.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonsSelectedEnd : MonoBehaviour
+{
+    public Image buttonExit;
+
+    public void ExitSelected()
+    {
+        buttonExit.GetComponent<Image>().color = new Color32(49, 137, 100, 255);
+    }
+    public void ExitDeselected()
+    {
+        buttonExit.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+    }
+}
+
+
