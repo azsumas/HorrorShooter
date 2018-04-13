654177b21eca67aa85eb2d08aa7dd02a9ae447b6
Ivan <iescario149@gmail.com>
2018-04-13 02:00:49 +0200

    Animatic Spanish and English work done :balloon:

--- Assets/Scripts/GameManager/Kinematic.cs
+++ Assets/Scripts/GameManager/Kinematic.cs
@@ -1,19 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(AudioSource))]
 
 public class Kinematic : MonoBehaviour
 {
     [Header("ChangeScene")]
     public float timeToChangeScreen;
     public bool loadNext = false;
-    [Header("EasingAtributes")]
     public float currentTime;
+    [Header("Resources")]
+    public MovieTexture kinSpa;
+    public MovieTexture kinEng;
+    private AudioSource audioMov;
+
+    private void Start()
+    {
+        if (Language.language == Language.Lang.esES)
+        {
+            GetComponent<RawImage>().texture = kinSpa as MovieTexture;
+            audioMov = GetComponent<AudioSource>();
+            audioMov.clip = kinSpa.audioClip;
+            kinSpa.Play();
+            audioMov.Play();
+        }
+        if (Language.language == Language.Lang.enUS)
+        {
+            GetComponent<RawImage>().texture = kinEng as MovieTexture;
+            audioMov = GetComponent<AudioSource>();
+            audioMov.clip = kinEng.audioClip;
+            kinEng.Play();
+            audioMov.Play();
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey) ChangeScene();
+        if (Input.anyKey)
+        {
+            if (Language.language == Language.Lang.esES)
+            {
+                kinSpa.Stop();
+                audioMov.Stop();
+            }
+            if (Language.language == Language.Lang.enUS)
+            {
+                kinEng.Stop();
+                audioMov.Stop();
+            }
+            ChangeScene();
+        }
 
         if (currentTime >= timeToChangeScreen) ChangeScene();
 
