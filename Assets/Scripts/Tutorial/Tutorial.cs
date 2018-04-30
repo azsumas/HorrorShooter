3d1ebb83cf549a82d2d5c6111188c81b7b549164
Ivan <iescario149@gmail.com>
2018-04-30 19:55:41 +0200

    Tutorial in progress

---
+++ Assets/Scripts/Tutorial/Tutorial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject textInitialization;
+    public GameObject player;
+    private inputManeger controlPlayer;
+    public GameObject ammoText;
+    public GameObject packEneText;
+    public GameObject energyBar;
+
+    public float timeCounter;
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.FindWithTag("Player");
+        controlPlayer = player.GetComponentInChildren<inputManeger>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timeCounter += Time.deltaTime;
+        if(timeCounter >= 7.8f)
+        controlPlayer.enabled = true;
+	}
+}
