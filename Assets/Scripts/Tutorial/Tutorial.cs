deac7dfe623f2f133f33b24acf25dd1baecbbd02
Ivan <iescario149@gmail.com>
2018-04-30 20:55:39 +0200

    Tutorial in progress

--- Assets/Scripts/Tutorial/Tutorial.cs
+++ Assets/Scripts/Tutorial/Tutorial.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class Tutorial : MonoBehaviour
     public GameObject packEneText;
     public GameObject energyBar;
 
+    public GameObject hudAmmo;
+    public Image ammoImage;
+
+    public GameObject radar;
+    private Image radarMask;
+
     public float timeCounter;
 
     // Use this for initialization
@@ -18,13 +25,23 @@ public class Tutorial : MonoBehaviour
     {
         player = GameObject.FindWithTag("Player");
         controlPlayer = player.GetComponentInChildren<inputManeger>();
+
+        hudAmmo = GameObject.FindWithTag("AmmoEnergy");
+        ammoImage = hudAmmo.GetComponentInChildren<Image>();
+
+        radar = GameObject.FindWithTag("Radar");
+        radarMask = radar.GetComponentInChildren<Image>();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
         timeCounter += Time.deltaTime;
-        if(timeCounter >= 7.8f)
-        controlPlayer.enabled = true;
-	}
+        if (timeCounter >= 3.0f) radarMask.color = new Vector4(75, 75, 75, 220);
+        if (timeCounter >= 4.0f) ammoImage.color = new Vector4(255, 255, 255, 255);
+        if (timeCounter >= 5.5f) ammoText.SetActive(true);
+        if (timeCounter >= 6.0f) packEneText.SetActive(true);
+        if (timeCounter >= 7.0f) energyBar.SetActive(true);
+        if(timeCounter >= 8.2f) controlPlayer.enabled = true;
+    }
 }
