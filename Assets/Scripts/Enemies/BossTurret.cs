1cd62dc4a65b3906475d6a2044c41a23488de357
Albert Rodriguez <albertrodriespada@gmail.com>
2018-05-07 01:56:07 +0200

    Code boss

---
+++ Assets/Scripts/Enemies/BossTurret.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTurret : MonoBehaviour {
+
+    public Transform target;
+
+    [Header("Attributes")]
+    public float range = 15f;
+    public GameObject player;
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
+
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+	
+    void UpdateTarget()
+    {
+        player = GameObject.FindGameObjectWithTag("player");
+        float shortestDistance = Mathf.Infinity;
+
+        float distanceToEnemy = Vector3.Distance(transform.position, player.transform.position);
+        if (distanceToEnemy < shortestDistance)
+        {
+            shortestDistance = distanceToEnemy;
+        }
+        if(shortestDistance <= range)
+        {
+            target = player.transform;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        if (target == null) return;
+        //TargetLock;
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime* turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+	}
+    
+    void Shoot()
+    {
+        Debug.Log("Shoot");
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ Assets/Scripts/Enemies/Bullet.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Transform target;
+
+    public float speed = 50f;
+    public GameObject impactEffect;
+
+    public void Seek (Transform _target)
+    {
+        target = _target;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+        void HitTarget()
+        {
+            GameObject effectsIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+            Destroy(effectsIns, 2f);
+            
+            Destroy(gameObject);
+        }
+	}
+}
