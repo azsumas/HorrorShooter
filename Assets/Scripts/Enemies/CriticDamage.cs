991c6154cd058545eefaed8a56eb51c10e28df73
Hector Bonada <hectorbonada@gmail.com>
2018-05-30 15:43:59 +0200

    bidones aplied

--- Assets/Scripts/Enemies/CriticDamage.cs
+++ Assets/Scripts/Enemies/CriticDamage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CriticDamage : MonoBehaviour {
-    public BossBehaviour bossScript;
+    public BossTurret bossScript;
     int hit = 20;
 	// Use this for initialization
 
