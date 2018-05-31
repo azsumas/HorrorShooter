94bc5e5c054f7249043f50b60a10a91b707a4160
Héctor Bonada <hectorbonada@gmail.com>
2018-05-31 23:15:26 +0200

    flash at the end of the level

--- Assets/Scripts/Environment/FinishLevel.cs
+++ Assets/Scripts/Environment/FinishLevel.cs
@@ -7,17 +7,34 @@ public class FinishLevel : MonoBehaviour
     public GameObject managerScene;
     private LevelManager script;
 
+    public Light myLight;
+
+    public bool changeIntesity = false;
+    public float intesitySpeed = 1.0f;
+    public float maxIntesity = 10.0f;
+
+    float startTime;
+
     private void Start()
     {
         managerScene = GameObject.FindWithTag("Manager");
         script = managerScene.GetComponent<LevelManager>();
+
+        myLight = GetComponent<Light>();
+        startTime = Time.time;
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            changeIntesity = true;
             script.LoadNext();
         }
-	}
+
+        if(changeIntesity)
+        {
+            myLight.intensity = Mathf.PingPong(Time.time * intesitySpeed, maxIntesity);
+        }
+    }
 }
---
+++ Assets/Scripts/lightEnd.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightEnd : MonoBehaviour {
+
+    public Light myLight;
+
+    public bool changeIntesity = false;
+    public float intesitySpeed = 1.0f;
+    public float maxIntesity = 10.0f;
+
+    float startTime;
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if(changeIntesity)
+        {
+            myLight.intensity = Mathf.PingPong(Time.time * intesitySpeed, maxIntesity);
+        }
+    }
+}
